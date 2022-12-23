92a5a5b79fefc9c11cc66a310d7d16fe71d0a504
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2022-12-24 01:51:30 +0200

    some necesseray modifications to the react app

--- modern_react_app_1/src/index.js
+++ modern_react_app_1/src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 const rootEle = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEle);
-
-function App() {
-  return <h1>Hello From React!</h1>;
-}
 
 root.render(<App />);
