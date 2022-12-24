c8e1c5b5e513bcdc784636136d18ebcf7f6b6daf
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2022-12-24 20:50:39 +0200

    finish project => card component

--- modern_react_app_1/src/App.js
+++ modern_react_app_1/src/App.js
@@ -1,16 +1,31 @@
 //! ─── Imports ────────────────────────────────────────────────────────────────
+import "./style/main.scss";
+//? ─── Import Components ───────────────────────────────────────────────────────
 import ProfileCard from "./ProfileCard";
+
+//? ─── Import Assets ───────────────────────────────────────────────────────────
+import alexaImg from "./assets/images/alexa.png";
+import cortonaImg from "./assets/images/cortana.png";
+import siriImg from "./assets/images/siri.png";
+
 //! ─── App Component ───────────────────────────────────────────────────────────
 
 function App() {
   return (
-    <>
-      <div>
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
+    <div className="container">
+      <h1>Personal Digital Assistant</h1>
+      <div className="row">
+        <div className="col-1-of-3">
+          <ProfileCard title="Alexa" handle="@alexa" imgSrc={alexaImg} />
+        </div>
+        <div className="col-1-of-3">
+          <ProfileCard title="Cortona" handle="@cortona" imgSrc={cortonaImg} />
+        </div>
+        <div className="col-1-of-3">
+          <ProfileCard title="Siri" handle="@siri" imgSrc={siriImg} />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- modern_react_app_1/src/ProfileCard/index.jsx
+++ modern_react_app_1/src/ProfileCard/index.jsx
@@ -1,9 +1,26 @@
 //! ─── Imports ─────────────────────────────────────────────────────────────────
-
+import "./styles.scss";
 //! ─── ProfileCard Component ───────────────────────────────────────────────────
 
-function ProfileCard() {
-  return <div>ProfileCard</div>;
+function ProfileCard({ title, handle, imgSrc }) {
+  return (
+    <div className="card">
+      <div className="card__profile">
+        <div className="card__image-box">
+          <img src={imgSrc} alt="pda logo" className="card__image" />
+        </div>
+        <div className="card__text">
+          <h2 className="card__title">This is {title}</h2>
+          <p className="card__social-handle"> {handle}</p>
+          <p className="card__description">
+            Amet duis commodo culpa esse excepteur incididunt ut incididunt nisi
+            commodo. Et in consectetur pariatur minim voluptate mollit
+            cupidatat. Ex sunt ex labore anim.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
