7de73227153ee1761d9e4658e94eeb87814145d6
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2022-12-26 01:22:02 +0200

    add app functionality

--- animals-modern_react_course_app_2/src/App.js
+++ animals-modern_react_course_app_2/src/App.js
@@ -1,10 +1,34 @@
-
-
-
+//! ─── Imports ─────────────────────────────────────────────────────────────────
 import "./App.scss";
+import { useState } from "react";
+
+//! ─── Import Components ───────────────────────────────────────────────────────
+import AnimalShow from "./components/AnimalShow";
+//! ─── App Component ───────────────────────────────────────────────────────────
+
+const getRandomAnimal = () => {
+  const animals = ["cat", "bird", "cow", "dog", "gator", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+};
 
 function App() {
-  return <div className="App">animals</div>;
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
+  return (
+    <div className="App">
+      <button onClick={handleClick}>Add Animal</button>
+
+      <div className="animals">{renderedAnimals}</div>
+    </div>
+  );
 }
 
 export default App;
--- animals-modern_react_course_app_2/src/components/AnimalShow/index.jsx
+++ animals-modern_react_course_app_2/src/components/AnimalShow/index.jsx
@@ -1,9 +1,42 @@
 //! ─── Imports ─────────────────────────────────────────────────────────────────
-
+import { useState } from "react";
+import "./styles.scss";
+//! ─── Assets Import ───────────────────────────────────────────────────────────
+import bird from "../../assets/svg/bird.svg";
+import cat from "../../assets/svg/cat.svg";
+import cow from "../../assets/svg/cow.svg";
+import dog from "../../assets/svg/dog.svg";
+import gator from "../../assets/svg/gator.svg";
+import horse from "../../assets/svg/horse.svg";
+import heart from "../../assets/svg/heart.svg";
 //! ─── AnimalShow Component ────────────────────────────────────────────────────
 
-function AnimalShow() {
-  return <div>AnimalShow Component</div>;
+const animalSVG = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+function AnimalShow({ type }) {
+  let [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count++);
+  };
+  return (
+    <div onClick={handleClick} className="animal">
+      <img src={animalSVG[type]} alt="animal" className="animal__image" />
+      <img
+        src={heart}
+        alt="heart"
+        style={{ width: 1 + 1 * count + "rem" }}
+        className="heart"
+      />
+    </div>
+  );
 }
 
 export default AnimalShow;
